87c06c4c0d8f1e8d5244ed4c9b77fc8aefa8bc75
Sara Terzoli <sara.terzoli@fht.org>
2025-10-15 16:22:36 +0200

    import missing package

--- modules/local/concatenate_vdj/resources/usr/bin/concatenate_vdj.py
+++ modules/local/concatenate_vdj/resources/usr/bin/concatenate_vdj.py
@@ -11,7 +11,7 @@ import glob
 import scanpy as sc                 # single-cell data processing
 import scirpy as ir                 # single-cell AIRR-data
 import anndata as ad                # store annotated matrix as anndata object
-
+import os
 
 warnings.filterwarnings("ignore")
 
